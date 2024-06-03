62d40f72bde772f5a92c7031fb5bfe3ce5b5c60f
Anusha-cr <anushacrramesh562@gmail.com>
2024-06-03 16:32:48 +0530

    busticket commit

---
+++ busticket.h.h
@@ -0,0 +1,9 @@
+#ifndef BUSTICKET_H_INCLUDED
+#define BUSTICKET_H_INCLUDED
+
+#include<stdio.h>
+int bookTickets(int bookedSeats, int totalSeats);
+int cancelTickets(int bookedSeats);
+void viewAvailableSeats(int bookedSeats, int totalSeats);
+
+#endif
