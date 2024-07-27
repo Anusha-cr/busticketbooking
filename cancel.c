321ad88b0dfced973ff8b8daf1ae5e061675a586
Anusha-cr <138802541+Anusha-cr@users.noreply.github.com>
2024-07-27 20:17:25 +0530

    Update cancel.c

--- cancel.c
+++ cancel.c
@@ -1,9 +1,11 @@
-#include "busticket.h"
-
 int cancelTickets(int bookedSeats) {
     int numTicketsToCancel;
     printf("Enter the number of tickets to cancel: ");
-    scanf("%d", &numTicketsToCancel);
+    if (scanf("%d", &numTicketsToCancel) != 1) {
+        printf("Invalid input. Please enter a number.\n");
+        while (getchar() != '\n');  // Clear input buffer
+        return bookedSeats;
+    }
     if (bookedSeats >= numTicketsToCancel) {
         bookedSeats -= numTicketsToCancel;
         printf("Ticket(s) cancelled successfully!\n");
