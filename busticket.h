fe68d4b1ddd1b341b51b44d258ee413941a1a796
Anusha-cr <138802541+Anusha-cr@users.noreply.github.com>
2024-07-28 14:05:01 +0530

    Update busticket.h

--- busticket.h
+++ busticket.h
@@ -3,9 +3,9 @@
 
 #include<stdio.h>
 void initializeSeats(int seats[][NUM_CITIES-1]);
-int bookTickets(int bookedSeats, int totalSeats);
-int cancelTickets(int bookedSeats);
-void viewAvailableSeats(int bookedSeats, int totalSeats);
+int bookTickets(int seats[][NUM_CITIES-1], float priceMatrix[NUM_CITIES][NUM_CITIES]);
+void cancelTickets(int seats[][NUM_CITIES-1]);
+void viewAvailableSeats(int seats[][NUM_CITIES-1]);
 void displayAvailableSeats(int seats[][NUM_CITIES-1], int departure, int destination);
 float calculatePrice(int departure, int destination, float priceMatrix[NUM_CITIES][NUM_CITIES]);
 
