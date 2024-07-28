57084099029540f92538614de256a9402ce7b65b
Anusha-cr <anushacrramesh562@gmail.com>
2024-07-28 13:53:51 +0530

    sixth commit

---
+++ price.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+float calculatePrice(int departure, int destination, float priceMatrix[NUM_CITIES][NUM_CITIES]) {
+    // Convert 1-based index to 0-based index
+    int depIndex = departure - 1;
+    int destIndex = destination - 1;
+    return priceMatrix[depIndex][destIndex];
+}
