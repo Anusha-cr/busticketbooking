51ad3c27faee033491a9540e33652d80b9af253a
Anusha-cr <138802541+Anusha-cr@users.noreply.github.com>
2024-07-28 14:24:26 +0530

    Update availableseat.c

--- availableseat.c
+++ availableseat.c
@@ -1,5 +1,10 @@
 #include "busticket.h"
 
-void viewAvailableSeats(int bookedSeats, int totalSeats) {
-    printf("Available seats: %d\n", totalSeats - bookedSeats);
+// Constants
+#define TOTAL_SEATS 50
+#define NUM_CITIES 5
+
+void viewAvailableSeats(int seats[][NUM_CITIES-1]) {
+    printf("Available seats:\n");
+    displayAvailableSeats(seats, 1, NUM_CITIES); // Display all available seats from start to end
 }
