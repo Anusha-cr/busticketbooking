50cf04f5ab8654dca8922bc0436859ebeaefb21a
Anusha-cr <138802541+Anusha-cr@users.noreply.github.com>
2024-07-28 14:28:58 +0530

    Update price.c

--- price.c
+++ price.c
@@ -1,5 +1,9 @@
 #include "busticket.h"
 
+// Constants
+#define TOTAL_SEATS 50
+#define NUM_CITIES 5
+
 float calculatePrice(int departure, int destination, float priceMatrix[NUM_CITIES][NUM_CITIES]) {
     // Convert 1-based index to 0-based index
     int depIndex = departure - 1;
