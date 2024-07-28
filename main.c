356ca16df470420b202e98b564354ba9b0519574
Anusha-cr <138802541+Anusha-cr@users.noreply.github.com>
2024-07-29 00:57:20 +0530

    Update main.c

--- main.c
+++ main.c
@@ -13,6 +13,17 @@
 #define TOTAL_SEATS 50
 #define NUM_CITIES 5
 
+// Function prototypes
+void initializeSeats(int seats[][NUM_CITIES-1]);
+int bookTickets(int seats[][NUM_CITIES-1], float priceMatrix[NUM_CITIES][NUM_CITIES], int busType);
+void cancelTickets(int seats[][NUM_CITIES-1], int busType);
+void viewAvailableSeats(int seats[][NUM_CITIES-1]);
+void displayAvailableSeats(int seats[][NUM_CITIES-1], int departure, int destination);
+float calculatePrice(int departure, int destination, float priceMatrix[NUM_CITIES][NUM_CITIES]);
+void writeBookingDetailsToFile(int busType, int departure, int destination, int seatNumber, float price);
+void writeCancellationDetailsToFile(int busType, int departure, int destination, int seatNumber);
+const char* getCityName(int cityIndex);
+
 int main() {
     // Arrays to store the availability of seats for Regular and Deluxe bus types
     int regularSeats[TOTAL_SEATS][NUM_CITIES-1];
