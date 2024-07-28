0255ac236c84ea466b258d27fb1d448205820821
Anusha-cr <138802541+Anusha-cr@users.noreply.github.com>
2024-07-29 00:18:12 +0530

    Update price.c

--- price.c
+++ price.c
@@ -5,8 +5,5 @@
 #define NUM_CITIES 5
 
 float calculatePrice(int departure, int destination, float priceMatrix[NUM_CITIES][NUM_CITIES]) {
-    // Convert 1-based index to 0-based index
-    int depIndex = departure - 1;
-    int destIndex = destination - 1;
-    return priceMatrix[depIndex][destIndex];
+    return priceMatrix[departure - 1][destination - 1];
 }
