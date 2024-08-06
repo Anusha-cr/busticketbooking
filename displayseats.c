dbe5025660561ff5614cfaee2a77aec54b0fe0fc
Anusha-cr <138802541+Anusha-cr@users.noreply.github.com>
2024-08-06 11:46:56 +0530

    Update displayseats.c

--- displayseats.c
+++ displayseats.c
@@ -6,7 +6,7 @@
 #define TOTAL_SEATS 50
 #define NUM_CITIES 5
 
-vvoid displayAvailableSeats(int seats[][NUM_CITIES-1], int departure, int destination) {
+void displayAvailableSeats(int seats[][NUM_CITIES-1], int departure, int destination) {
     printf("Available seats from %s to %s:\n", getCityName(departure), getCityName(destination));
     for (int i = 0; i < TOTAL_SEATS; i++) {
         printf("Seat %d:", i + 1);
